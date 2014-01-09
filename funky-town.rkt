e8ed61e8cccf48023802a49d23b533eaa62f6360
Vincent St-Amour <stamourv@racket-lang.org>
2014-01-09 18:08:23 -0500

    Add funky town.

---
+++ funky-town.rkt
@@ -0,0 +1,14 @@
+#lang racket/base
+
+(require "synth/main.rkt")
+
+(time
+ (emit
+  (mix
+   (sequence
+     sawtooth-wave #:bpm 380 #:times 2
+     [(C 5) #f (C 5) #f (A# 4) #f (C 5) (#f 3) (G 4) (#f 3)
+      (G 4) #f (C 5) #f (F 5) #f (E 5) #f (C 5) (#f 9)])
+   (drum #:bpm 380 #:times 8 (O #f #f #f X #f #f #f)))
+  "funky-town.wav"))
+(delete-file "funky-town.wav")
