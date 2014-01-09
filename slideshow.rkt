266fb6bd007e95e6a9e27988e2df596a79101b39
Vincent St-Amour <stamourv@racket-lang.org>
2014-01-09 15:58:14 -0500

    Fix bitrot in slideshow.

--- slideshow.rkt
+++ slideshow.rkt
@@ -3,5 +3,5 @@
 (require "utils.rkt")
 
 (call-with-frozen-collects
- "./slideshow/" '("slideshow" "pict" "racket/draw")
+ "./slideshow/" '("slideshow" "pict" "racket/draw" "racket/snip" "mred" "texpict")
  (lambda () (dynamic-require "slideshow/main.rkt" #f)))
